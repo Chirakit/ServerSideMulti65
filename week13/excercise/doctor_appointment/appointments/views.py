a517d6669d6f7d685fb1b6116681aeded2a64451
Chirakit <65070034@kmitl.ac.th>
2024-10-10 11:24:53 +0700

    archive

--- week13/excercise/doctor_appointment/appointments/views.py
+++ week13/excercise/doctor_appointment/appointments/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from appointments.models import Doctor, Patient, Appointment
 from appointments.serializers import DoctorSerializer, PatientSerializer, AppointmentSerializer, AppointmentAddSerializer
-from django.http import Http404
+from django.http import Http404, HttpResponse, JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.parsers import JSONParser
 
 class DoctorList(APIView):
     def get(self, request):
@@ -35,3 +36,18 @@ class AppointmentList(APIView):
 class AppointmentDetailList(APIView):
     def get(self, request, appointment_id):
         appointment_detail = Appointment.objects.get(pk=appointment_id)
+        serializer = AppointmentSerializer(appointment_detail)
+        return Response(serializer.data)
+
+    def put(self, request, appointment_id):
+        appointment_detail = Appointment.objects.get(pk=appointment_id)
+        serializer = AppointmentAddSerializer(appointment_detail, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, appointment_id):
+        appointment_detail = Appointment.objects.get(pk=appointment_id)
+        appointment_detail.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
